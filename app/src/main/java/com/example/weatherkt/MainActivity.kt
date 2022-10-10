86e3e7ed49e3b26d4154959a6e62042ae48edbdc
SainiShivanshu <shivanshusaini216@gmail.com>
2022-10-10 22:23:55 +0530

    fina;

--- app/src/main/java/com/example/weatherkt/MainActivity.kt
+++ app/src/main/java/com/example/weatherkt/MainActivity.kt
@@ -1,24 +1,23 @@
 package com.example.weatherkt
 
 import android.content.Intent
-import android.graphics.Color
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
-
 import kotlinx.android.synthetic.main.activity_main.*
 import org.json.JSONObject
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,6 +25,35 @@ class MainActivity : AppCompatActivity() {
         val long = intent.getStringExtra("long")
 
         getJsonData(lat,long)
+
+
+        search.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+                val city = cityEdt.text.toString()
+                if (city.isEmpty()) {
+                    Toast.makeText(this@MainActivity, "Please enter city Name", Toast.LENGTH_SHORT)
+                        .show()
+                } else {
+        getWeatherInfo(city)
+                }
+            }
+        })
+    }
+
+    private fun getWeatherInfo(city: String) {
+        val API_KEY = "d0f056b51d4becc728d4a078dd2d1002"
+        val queue = Volley.newRequestQueue(this)
+        val url = "https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}"
+        val jsonRequest = JsonObjectRequest(
+            Request.Method.GET, url,null,
+            Response.Listener { response ->
+                setValues(response)
+            },
+            Response.ErrorListener { Toast.makeText(this,"Please restart the app again!",Toast.LENGTH_LONG).show() })
+
+// Add the request to the RequestQueue.
+        queue.add(jsonRequest)
+
     }
 
     private fun getJsonData(lat: String?, long: String?) {
@@ -64,7 +92,7 @@ val API_KEY = "d0f056b51d4becc728d4a078dd2d1002"
 
                 var mintemp=response?.getJSONObject("main")?.getString("temp_min")
                 mintemp=((((mintemp)?.toFloat()!!-273.15)).toInt()).toString()
-                min_temp.text=mintemp+"°C"
+                min_temp.text=mintemp+"°C"+" -"
                 var maxtemp=response?.getJSONObject("main")?.getString("temp_max")
                 maxtemp=((ceil((maxtemp)?.toFloat()!! -273.15)).toInt()).toString()
                 max_temp.text=maxtemp+"°C"
@@ -81,7 +109,11 @@ val API_KEY = "d0f056b51d4becc728d4a078dd2d1002"
 
 
 
+
     }
+
+
+
 
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -101,4 +133,9 @@ val API_KEY = "d0f056b51d4becc728d4a078dd2d1002"
              }
         return super.onOptionsItemSelected(item)
     }
+
+
+
+
 }
+
